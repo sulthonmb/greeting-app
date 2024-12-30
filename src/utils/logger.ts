2abac034dde19c7e82921c48a67911f628aecf18
Sulthonul Mubarok <sulthonmb@gmail.com>
2024-12-30 18:51:41 +0700

    feature(logs): fix loki configuration

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,16 +3,16 @@ import LokiTransport from "winston-loki";
 import env from "../config/env";
 
 const loggingWinston = new LokiTransport({
-    host: env.LOKI_HOST,
-    labels: {
-        app: env.APP_NAME,
-    },
-    json: true,
-    basicAuth: 'USER_ID:GRAFANA_CLOUD_TOKEN',
-    format: winston.format.json(),
-    replaceTimestamp: true,
-    onConnectionError: (err) => console.error(err),
-})
+  host: env.LOKI_HOST,
+  labels: {
+    app: env.APP_NAME,
+  },
+  json: true,
+  basicAuth: "USER_ID:GRAFANA_CLOUD_TOKEN",
+  format: winston.format.json(),
+  replaceTimestamp: true,
+  onConnectionError: (err) => console.error(err),
+});
 
 const loggerUtil = winston.createLogger({
   transports: [
